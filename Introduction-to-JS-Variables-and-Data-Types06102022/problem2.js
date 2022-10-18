7faa90a63dcc8590303d606fda35cb98f878b85d
keshav0605 <keshav06patel.com>
2022-10-18 09:39:51 +0530

    Assignment 1

---
+++ Introduction-to-JS-Variables-and-Data-Types06102022/problem2.js
@@ -0,0 +1,22 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+// Declaring Variable
+var name;
+
+// Assigning Value
+name = "Keshav Patel";
+
+// Changing Variable Value to Store Father Name
+name = "Kantilal Patel";
+
+// Again changing the Variable Value to Store Father Name
+name = "Preeti Patel"
+
+// Printing the Results
+console.log(name);
---
+++ Introduction-to-JS-Variables-and-Data-Types06102022/problem4.js
@@ -0,0 +1,52 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+h1 ="██████╗░███████╗░██████╗██╗░░░██╗██╗░░░░░████████╗"
+h2 ="██╔══██╗██╔════╝██╔════╝██║░░░██║██║░░░░░╚══██╔══╝"
+h3 ="██████╔╝█████╗░░╚█████╗░██║░░░██║██║░░░░░░░░██║░░░"
+h4 ="██╔══██╗██╔══╝░░░╚═══██╗██║░░░██║██║░░░░░░░░██║░░░"
+h5 ="██║░░██║███████╗██████╔╝╚██████╔╝███████╗░░░██║░░░"
+h6 ="╚═╝░░╚═╝╚══════╝╚═════╝░░╚═════╝░╚══════╝░░░╚═╝░░░"
+
+
+var Name;
+Name = "Student Name    - Keshav Patel";
+
+var School;
+School = "School Name     - Chameli Devi Public School";
+
+
+var Grade;
+Grade = "Grade           - A+";
+
+var Section;
+Section = "Class & Section - 12th G";
+
+var RollNumber;
+RollNumber = "Roll Number     - 21";
+
+var MarksScoredinEnglish;
+MarksScoredinEnglish = "ENGLISH         - 96";
+
+var MarksScoredinHindi;
+MarksScoredinHindi = "HINDI           - 95";
+
+var MarksScoredinMathematics;
+MarksScoredinMathematics = "MATHEMATICS     - 99"
+
+console.log(h1);
+console.log(h2);
+console.log(h3);
+console.log(h4);
+console.log(h5);
+console.log(h6);
+console.log(Name);
+console.log(School);
+console.log(Grade);
+console.log(Section);
+console.log(RollNumber);
+console.log(MarksScoredinEnglish);
+console.log(MarksScoredinHindi);
+console.log(MarksScoredinMathematics);
